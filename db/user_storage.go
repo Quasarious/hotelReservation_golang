6d02f0b41073694b89c01370d27f88e68a465aaf
unknown <miftaxutdinov_2002@mail.ru>
2023-08-05 00:31:30 +0300

    db added

---
+++ db/user_storage.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"hotelReservation_golang/types"
+)
+
+const userColl = "users"
+
+type UserStorage interface {
+	GetUserByID(context.Context, string) (*types.User, error)
+}
+
+type MongoUserStore struct {
+	client *mongo.Client
+	coll   *mongo.Collection
+}
+
+func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
+	return &MongoUserStore{
+		client: client,
+		coll:   client.Database(DBNAME).Collection(userColl),
+	}
+}
+
+func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	var user types.User
+	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
